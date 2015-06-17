6a31dd4bb731f5367ae26f22f7d7033809f11c0b
Michael Ganger <mganger747@gmail.com>
2015-06-16 21:58:10 -0400

    [TICKET-01b2e7]

--- gitticket/__init__.py
+++ gitticket/__init__.py
@@ -1,1 +1,31 @@
 #This is the main interface to the package
+
+from git import Repo
+import os
+active_repo = Repo(os.getcwd())
+
+
+import sys
+args = sys.argv[1:]
+#might want to change in the future
+exit = sys.exit
+
+#Structure of a git ticket statement:
+#
+#    git ticket let <id> need <id>
+#    ---------- --- ---- ---- ----
+#      subject  verb s   v    s
+#
+# - git ticket is obviously an implicit subject
+# - the first verb is the command
+# - if the command warrants it, you can chain alternating subjects and verbs
+
+command = None
+subject = None
+verb    = None
+
+try:
+	command = args[0]
+	subject = args[1::2]
+	verb    = args[2::2]
+except: pass
